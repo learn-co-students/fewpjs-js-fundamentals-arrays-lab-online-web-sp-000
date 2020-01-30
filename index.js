987b0d33617c7a0273d970bac4201c3dc928a435
Kenton Archer <kenton8654@yahoo.com>
2020-01-29 19:37:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+const append = ["Milo","Otis","Garfield"]
+append.push("Odie")
+// *pushes* elements onto
+// the *ends* of arrays
+
+const prepend = ["Milo","Otis","Garfield"]
+prepend.unshift("Odie")
+// unshift()` *adds*
+// elements to the *beginnings* of arrays
+
+const removeLast = ["Milo","Otis","Garfield"]
+removeLast.pop()
+// Takes off elements from the end of the array
+
+
+const removeFirst = ["Milo","Otis","Garfield"]
+removeFirst.shift()
+// Removes of the element in the beginning of the array. 
