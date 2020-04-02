d549fc75181ac4c416ee5ca4caf2ada665e89f3a
Patrice Drayton <draytonpatrice@marilynssecret.org>
2020-04-02 16:05:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"],
+      prepend = append.slice(),
+      removeLast = append.slice(),
+      removeFirst = append.slice();
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
