ba52991b531099476084a285009365ce49d3e6ae
doakcovington <doakcovington@protonmail.com>
2020-05-28 16:03:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+
+const append = ["Milo", "Otis", "Garfield"];
+
+const prepend = ["Milo", "Otis", "Garfield"];
+
+const removeLast = ["Milo", "Otis", "Garfield"];
+
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+//Append "Odie" to the end of append
+append.push("Odie");
+
+//Prepend "Odie" to the beginning of prepend
+prepend.unshift('Odie');
+
+//Remove the last pet from removeLast
+removeLast.pop();
+
+//Remove the first pet from removeFirst
+removeFirst.shift();
