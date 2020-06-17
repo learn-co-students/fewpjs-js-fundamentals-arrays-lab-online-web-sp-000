0c59587b3431c5a93c8676419898cbeeda6dc24b
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-06-17 21:46:03 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+// "Append" means "add to the end" and "prepend" means "add to the beginning."
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+// Append the pet "Odie" to the end of append.
+append.push("Odie");
+// Prepend the pet "Odie" to the beginning of prepend.
+prepend.unshift("Odie");
+// Remove the last pet from removeLast.
+removeLast.pop();
+// Remove the first pet from removeFirst.
+removeFirst.shift();
