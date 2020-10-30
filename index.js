b5de3d5748fc6cc276de8e72d789f76a9a96326c
Joseph <DeGregorio.Joseph@gmail.com>
2020-10-30 13:01:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+let pets = ["Milo", "Otis", "Garfield"]
+
+const append = ["Milo", "Otis", "Garfield"]
+append.push("Odie")
+
+const prepend = ["Milo", "Otis", "Garfield"]
+prepend.unshift("Odie")
+
+const removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop()
+
+const removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift()
