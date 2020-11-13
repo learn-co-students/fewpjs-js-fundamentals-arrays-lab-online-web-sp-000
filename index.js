7d0615a0697baa0f12dce0ffafabec5c3880e860
Meisuleen Kased <meisuleen@gmail.com>
2020-11-13 04:10:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Write your solution here!
+<<<<<<< HEAD
 const append = ["Milo", "Otis", "Garfield"];
+=======
+const = append ["Milo", "Otis", "Garfield"]
+>>>>>>> 2ff16599a1dc165bc638fa42f7103ab8d5181fea
 append.push('Odie');
 append;
 
 
+<<<<<<< HEAD
 const prepend = ["Milo", "Otis", "Garfield"];
 prepend.unshift('Odie');
 prepend;
@@ -13,5 +18,16 @@ removeLast.pop();
 removeLast;
 
 const removeFirst = ["Milo", "Otis", "Garfield"];
+=======
+const = prepend ["Milo", "Otis", "Garfield"]
+prepend.unshift('Odie');
+prepend;
+
+const = removeLast ["Milo", "Otis", "Garfield"]
+removeLast.pop();
+removeLast;
+
+const = removeFirst ["Milo", "Otis", "Garfield"]
+>>>>>>> 2ff16599a1dc165bc638fa42f7103ab8d5181fea
 removeFirst.shift();
 removeFirst;
