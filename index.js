c758d496f53cb3839aca71a844559babb9c4c5cc
zacharytq <zacharytq@gmail.com>
2020-11-20 10:35:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const initialArray = ["Milo", "Otis", "Garfield"];
+
+const append = [...initialArray, "Odie"]
+
+const prepend = ["Odie", ...initialArray]
+
+const removeLast = ["Milo", "Otis"]
+
+const removeFirst = ["Otis", "Garfield"]
