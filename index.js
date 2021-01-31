1b3d6c56239059421202dd744525391a3f224abc
khoinguyenkc <khoinguyenkc@gmail.com>
2021-01-31 10:17:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+append = ["Milo", "Otis", "Garfield"];
+prepend = ["Milo", "Otis", "Garfield"];
+removeLast = ["Milo", "Otis", "Garfield"];
+removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");
+prepend.unshift("Oddie");
+removeLast.pop();
+removeFirst.shift();
